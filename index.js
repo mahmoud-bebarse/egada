ae802d605fdcb99ce9f8198a3b24d36f54266539
mahmoud-bebarse <87090355+mahmoud-bebarse@users.noreply.github.com>
2023-04-18 13:02:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
 // adding morgan to log HTTP requests
 if (process.env.NODE_ENV !== "production") app.use(morgan("dev"));
