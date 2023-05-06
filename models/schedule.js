c36f2baa103d0401e7e8885339c11ae0cd271ea4
Mahmoud Baybars <mahmoudbaybars213@gmail.com>
2023-05-06 15:16:40 +0300

    small change

--- models/schedule.js
+++ models/schedule.js
@@ -1,19 +1,17 @@
 const mongoose = require("mongoose");
 
-const scheduleSchema = new mongoose.Schema(
-  {
-    doctor: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "doctor",
-    },
-    fromHr: { type: Number, required: true, default: 0 },
-    fromMin: { type: Number, required: false, default: 0 },
-    toHr: { type: Number, required: true, default: 0 },
-    toMin: { type: Number, required: false, default: 0 },
-    day: { type: Number, required: true, default: 0 },
-    status: { type: Boolean, default: true },
-    entryDate: { type: Date, default: Date.now },
-  }
-);
+const scheduleSchema = new mongoose.Schema({
+  doctor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "doctor",
+  },
+  fromHr: { type: Number, required: true, default: 0, min: 0, max: 23 },
+  fromMin: { type: Number, required: false, default: 0, min: 0, max: 59 },
+  toHr: { type: Number, required: true, default: 0, min: 0, max: 23 },
+  toMin: { type: Number,required: false,default: 0, min: 0, max: 59,},
+  day: { type: Number, required: true, default: 0, min: 0, max: 7 },
+  status: { type: Boolean, default: true },
+  entryDate: { type: Date, default: Date.now },
+});
 const Schedule = mongoose.model("schedule", scheduleSchema);
 module.exports = Schedule;
