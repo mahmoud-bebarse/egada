59764ff6166f36070e82db8ba3d92ae2c1404377
Mahmoud Baybars <mahmoudbaybars213@gmail.com>
2023-07-04 20:56:47 +0300

    delete

--- routes/image.js
+++ routes/image.js
@@ -9,6 +9,6 @@ router.post("/", single, imageController.postImage);
 router.delete("/:id", imageController.deleteImage);
 router.get("/:id", imageController.getImageById);
 router.put("/:id", imageController.updateImage);
-router.delete("/all", imageController.deleteAll);
+router.delete("/delete/all", imageController.deleteAll);
 
 module.exports = router;
